91b1807ce44fa669703959bb52d579ae9992d73c
MoonTahoe <alex@moonhighway.com>
2018-09-14 12:20:24 -0700

    Initial Apollo Server Setup

--- src/index.js
+++ src/index.js
@@ -1,38 +1,20 @@
-const express = require('express')
-const expressPlayground = require('graphql-playground-middleware-express').default
-const { ApolloServer } = require('apollo-server-express')
-const { readFileSync } = require('fs')
-const { MongoClient } = require('mongodb')
+const { ApolloServer } = require('apollo-server')
 
-const resolvers = require('./resolvers')
-const typeDefs = readFileSync('src/typeDefs.graphql', 'UTF-8')
-
-const start = async (port) => {
-
-    const client = await MongoClient.connect(process.env.DB_HOST, { useNewUrlParser: true })
-    const db = client.db()
-
-    const context = {
-        photos: db.collection('photos'),
-        users: db.collection('users'),
-        currentUser: null
+const typeDefs = `
+    type Query {
+        gnar: String
     }
-    
-    const server = new ApolloServer({ typeDefs, resolvers, context })
-    
-    const app = express()
-    server.applyMiddleware({app})
+`
 
-    app.get('/playground', expressPlayground({ endpoint: '/graphql' }))
-
-    app.get('/', (req, res) => {
-        res.end(`Welcome to the Photo Share API`)
-    })
-
-    app.listen({ port }, () => {
-        console.log(`photo-share api running on port ${port}`)
-    })
-
+const resolvers = {
+    Query: {
+        gnar: () => 'gnarly!!!'
+    }
 }
 
-start(process.env.PORT || 4000)
+const server = new ApolloServer({ typeDefs, resolvers })
+
+server.listen()
+    .then(({port}) => `server listening on ${port}`)
+    .then(console.log)
+    .catch(console.error)
