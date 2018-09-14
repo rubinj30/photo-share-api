ef49cf3602c78c52c306d2fc501869631b269ba3
MoonTahoe <alex@moonhighway.com>
2018-09-14 16:49:38 -0700

    adds .env and refactors code into seperate files

---
+++ src/resolvers.js
@@ -0,0 +1,44 @@
+const { generate } = require('short-id')
+
+const photos = require('../data/photos')
+const users = require('../data/users')
+
+module.exports = {
+
+    Query: {
+        totalPhotos: () => photos.length,
+        allPhotos: () => photos,
+        Photo: (parent, { id }) => photos.find(p => p.id === id),
+        totalUsers: () => users.length,
+        allUsers: () => users,
+        User: (parent, { id }) => users.find(p => p.id === id)
+    },
+
+    Mutation: {
+        postPhoto: (parent, { input }, { currentUser }) => {
+
+            if (!currentUser) {
+                throw new Error(`Only authorized users can post photos`)
+            }
+
+            const id = generate()
+            const newPhoto = { 
+                id, 
+                ...input,
+                userID: currentUser.id 
+            }
+            photos.push(newPhoto)
+            return newPhoto
+        }
+    },
+
+    Photo: {
+        url: parent => `/img/photos/${parent.id}.jpg`,
+        postedBy: parent => users.find(u => u.id === parent.userID)
+    },
+
+    User: {
+        postedPhotos: parent => photos.filter(p => p.userID === parent.id)
+    }
+
+}
