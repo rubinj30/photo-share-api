46a6fb7002d2aefd74a843c64f23407d63238ebd
MoonTahoe <alex@moonhighway.com>
2018-09-14 12:50:49 -0700

    posting photo meta data

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 const { ApolloServer } = require('apollo-server')
+const { generate } = require('short-id')
 
 const typeDefs = `
+
+    type Photo {
+        id: ID!
+        name: String!
+        description: String
+    }
+
     type Query {
         totalPhotos: Int!
     }
+
+    type Mutation {
+        postPhoto(name: String!, description: String): Photo!
+    }
+
 `
 
 const photos = []
@@ -11,6 +24,14 @@ const photos = []
 const resolvers = {
     Query: {
         totalPhotos: () => photos.length
+    },
+    Mutation: {
+        postPhoto: (parent, args) => {
+            const id = generate()
+            const newPhoto = { id, ...args }
+            photos.push(newPhoto)
+            return newPhoto
+        }
     }
 }
 
