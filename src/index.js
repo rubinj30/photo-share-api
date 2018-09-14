58af9e94338adbff03cccfe3d8c82a57cd7c151c
MoonTahoe <alex@moonhighway.com>
2018-09-14 12:20:24 -0700

    Initial Apollo Server Setup

--- src/index.js
+++ src/index.js
@@ -1,37 +1,14 @@
 const { ApolloServer } = require('apollo-server')
-const { generate } = require('short-id')
 
 const typeDefs = `
-
-    type Photo {
-        id: ID!
-        name: String!
-        description: String
-    }
-
     type Query {
-        totalPhotos: Int!
+        gnar: String
     }
-
-    type Mutation {
-        postPhoto(name: String! description: String): Photo!
-    }
-
 `
-
-const photos = []
 
 const resolvers = {
     Query: {
-        totalPhotos: () => photos.length
-    },
-    Mutation: {
-        postPhoto: (parent, args) => {
-            const id = generate()
-            const newPhoto = { id, ...args }
-            photos.push(newPhoto)
-            return newPhoto
-        }
+        gnar: () => 'gnarly!!!'
     }
 }
 
