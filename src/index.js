65356d7cc0f4544ea23ce8564e48b8f05ee1d684
MoonTahoe <alex@moonhighway.com>
2018-09-14 12:20:24 -0700

    Initial Apollo Server Setup

--- src/index.js
+++ src/index.js
@@ -1,52 +1,20 @@
-const express = require('express')
-const { createServer } = require('http')
-const expressPlayground = require('graphql-playground-middleware-express').default
-const { ApolloServer, PubSub } = require('apollo-server-express')
-const { readFileSync } = require('fs')
-const { MongoClient } = require('mongodb')
+const { ApolloServer } = require('apollo-server')
 
-const resolvers = require('./resolvers')
-const typeDefs = readFileSync('src/typeDefs.graphql', 'UTF-8')
-
-const start = async (port) => {
-
-    const client = await MongoClient.connect(process.env.DB_HOST, { useNewUrlParser: true })
-    const db = client.db()
-    const pubsub = new PubSub()
-
-    const context = async ({ req, connection }) => {
-        const photos = db.collection('photos')
-        const users = db.collection('users')
-        const githubToken = req ? req.headers.authorization : connection.context.Authorization
-        const currentUser = await users.findOne({ githubToken })
-        return { photos, users, currentUser, pubsub }
+const typeDefs = `
+    type Query {
+        gnar: String
     }
-    
-    const server = new ApolloServer({ typeDefs, resolvers, context })
-    
-    const app = express()
-    server.applyMiddleware({app})
-    
-    app.get('/playground', expressPlayground({ 
-        endpoint: '/graphql',
-        subscriptionEndpoint: '/graphql' 
-    }))
+`
 
-    app.get('/', (req, res) => {
-        let url = `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}&scope=user`
-        res.end(`
-            <h1>Welcome to the Photo Share API</h1>
-            <a href="${url}">Request a GitHub Code</a>
-        `)
-    })
-
-    const httpServer = createServer(app)
-    server.installSubscriptionHandlers(httpServer)
-
-    httpServer.listen({ port }, () => {
-        console.log(`photo-share api running on port ${port}`)
-    })
-
+const resolvers = {
+    Query: {
+        gnar: () => 'gnarly!!!'
+    }
 }
 
-start(process.env.PORT || 4000)
+const server = new ApolloServer({ typeDefs, resolvers })
+
+server.listen()
+    .then(({port}) => `server listening on ${port}`)
+    .then(console.log)
+    .catch(console.error)
