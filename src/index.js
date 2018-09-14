63eac0b165a6d4b913f675f626d021eb76cdbb09
MoonTahoe <alex@moonhighway.com>
2018-09-14 12:20:24 -0700

    Initial Apollo Server Setup

--- src/index.js
+++ src/index.js
@@ -1,28 +1,20 @@
 const { ApolloServer } = require('apollo-server')
-const { readFileSync } = require('fs')
-const { MongoClient } = require('mongodb')
 
-const resolvers = require('./resolvers')
-const typeDefs = readFileSync('src/typeDefs.graphql', 'UTF-8')
-
-const start = async () => {
-
-    const client = await MongoClient.connect(process.env.DB_HOST, { useNewUrlParser: true })
-    const db = client.db()
-
-    const context = {
-        photos: db.collection('photos'),
-        users: db.collection('users'),
-        currentUser: null
+const typeDefs = `
+    type Query {
+        gnar: String
     }
-    
-    const server = new ApolloServer({ typeDefs, resolvers, context })
-    
-    server.listen()
-        .then(({port}) => `server listening on ${port}`)
-        .then(console.log)
-        .catch(console.error)
+`
 
+const resolvers = {
+    Query: {
+        gnar: () => 'gnarly!!!'
+    }
 }
 
-start()
+const server = new ApolloServer({ typeDefs, resolvers })
+
+server.listen()
+    .then(({port}) => `server listening on ${port}`)
+    .then(console.log)
+    .catch(console.error)
