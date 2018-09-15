68fa1c685abae6e38a6f3d35a398ed3040b0b58c
MoonTahoe <alex@moonhighway.com>
2018-09-14 17:03:47 -0700

    Configures MongoDB database

--- src/index.js
+++ src/index.js
@@ -1,18 +1,28 @@
 const { ApolloServer } = require('apollo-server')
 const { readFileSync } = require('fs')
+const { MongoClient } = require('mongodb')
 
 const resolvers = require('./resolvers')
 const typeDefs = readFileSync('src/typeDefs.graphql', 'UTF-8')
 
-console.log('database host: ', process.env.DB_HOST)
+const start = async () => {
 
-const context = {
-    currentUser: null
+    const client = await MongoClient.connect(process.env.DB_HOST, { useNewUrlParser: true })
+    const db = client.db()
+
+    const context = {
+        photos: db.collection('photos'),
+        users: db.collection('users'),
+        currentUser: null
+    }
+    
+    const server = new ApolloServer({ typeDefs, resolvers, context })
+    
+    server.listen()
+        .then(({port}) => `server listening on ${port}`)
+        .then(console.log)
+        .catch(console.error)
+
 }
 
-const server = new ApolloServer({ typeDefs, resolvers, context })
-
-server.listen()
-    .then(({port}) => `server listening on ${port}`)
-    .then(console.log)
-    .catch(console.error)
+start()
